9f642b29311791a3495a9e21ccd54323b9d497f2
i544693 <koy@ko8e24.top>
2022-08-18 14:46:51 +0800

    add: widget.

---
+++ src/components/common/GithubIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- <el-badge value="hot" class="item"> -->
+  <div>
+    <a href="https://github.com/Koooooo-7/KaViewer" target="_blank"
+      ><img
+        alt="GitHub Stars"
+        src="https://img.shields.io/github/stars/Koooooo-7/KaViewer?label=GITHUB%20%20%20%20STARTS"
+    /></a>
+    <a href="https://github.com/Koooooo-7/KaViewer" target="_blank"
+      ><img
+        alt="GitHub License"
+        src="https://img.shields.io/github/license/Koooooo-7/KaViewer?label=LICENSE"
+    /></a>
+  </div>
+    <!-- </el-badge> -->
+</template>
+<script>
+export default {
+  name: "GithubIcon",
+};
+</script>
