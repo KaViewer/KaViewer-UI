b241f8218b8e9cd3c19ee1ef1138819020f45041
i544693 <koy@ko8e24.top>
2022-08-18 15:47:15 +0800

    add: logo.

--- src/components/common/Logo.vue
+++ src/components/common/Logo.vue
@@ -1,10 +1,19 @@
 <template>
   <el-row id="logo-kaviewer">
-    <!-- <el-col :span="3"
-      ><div class="grid-content bg-purple"></div>
-      Icon</el-col
-    > -->
-    <el-col :span="12"
+    <el-col :span="2"
+      ><div class="grid-content bg-purple">
+<a href="https://github.com/Koooooo-7/KaViewer" target="_blank"
+      >
+        <el-image
+      style="width: 50px; height: 50px"
+      :src="src"
+      fit="cover"></el-image>
+</a>
+
+      </div>
+      </el-col
+    >
+    <el-col :span="2" :offset="1"
       ><div >
         <div>KaViewer</div>
       </div></el-col
@@ -12,12 +21,19 @@
   </el-row>
 </template>
 <script>
+import icon from '../../asset/icon.png'
 export default {
   name: "Logo",
+  data() {
+      return {
+        src: icon 
+      }
+    }
 };
 </script>
 <style scoped>
 #logo-kaviewer {
-  font-size: 50px;
+  font-size: 52px;
+  color: #409EFF;
 }
 </style>
