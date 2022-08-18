5928bcf03a52c37294251506c62c3e1d0e7b370e
i544693 <koy@ko8e24.top>
2022-08-18 14:47:14 +0800

    add: draft icon.

---
+++ src/components/common/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-row id="logo-kaviewer">
+    <!-- <el-col :span="3"
+      ><div class="grid-content bg-purple"></div>
+      Icon</el-col
+    > -->
+    <el-col :span="12"
+      ><div >
+        <div>KaViewer</div>
+      </div></el-col
+    >
+  </el-row>
+</template>
+<script>
+export default {
+  name: "Logo",
+};
+</script>
+<style scoped>
+#logo-kaviewer {
+  font-size: 50px;
+}
+</style>
